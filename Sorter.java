e1203c9013a6961c43ad686fd935cdd524eea01e
Seifo100 <seif.mahmoud1011@gmail.com>
2023-09-27 19:49:49 +0300

    selection sort

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,27 @@
 public class Sorter {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            int[] arr = {64, 25, 12, 22, 11};
+            selectionSort(arr);
+            System.out.println("Sorted array:");
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+        }
     
-   } 
-}
+        public static void selectionSort(int[] arr) {
+            int n = arr.length;
+    
+            for (int i = 0; i < n - 1; i++) {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+                // Swap the found minimum element with the element at index i
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+   }
