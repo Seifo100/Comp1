cc586681a49610d26395271e53d3c3afcf16064b
Seifo100 <seif.mahmoud1011@gmail.com>
2023-09-27 19:54:28 +0300

    insertion sort

--- Sorter.java
+++ Sorter.java
@@ -1,11 +1,19 @@
 public class Sorter {
         public static void main(String[] args) {
-            int[] arr = {64, 25, 12, 22, 11};
-            selectionSort(arr);
+            int[] arr1 = {64, 25, 12, 22, 11};
+            selectionSort(arr1);
             System.out.println("Sorted array:");
-            for (int i : arr) {
+            for (int i : arr1) {
                 System.out.print(i + " ");
             }
+            int[] arr2 = {12, 11, 13, 5, 6};
+        System.out.println("");
+        insertionSort(arr2);
+
+        System.out.println("Sorted array:");
+        for (int num : arr2) {
+            System.out.print(num + " ");
+        }
         }
     
         public static void selectionSort(int[] arr) {
@@ -24,4 +32,19 @@ public class Sorter {
                 arr[i] = temp;
             }
         }
-   }
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
